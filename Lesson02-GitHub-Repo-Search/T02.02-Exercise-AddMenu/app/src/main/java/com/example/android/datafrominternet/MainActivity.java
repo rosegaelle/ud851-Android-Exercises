c12358e4aeaebcced874bacc127eaa45ed361180
MIrrorIMage <MIrrorIMage@rosegaelle.com>
2018-08-11 16:20:36 -0400

    T02.02 TODOs 11 through 15.

--- Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.02-Exercise-AddMenu/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -15,11 +15,14 @@
  */
 package com.example.android.datafrominternet;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -43,12 +46,27 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main, menu);
-        return true; // displayedMenu-->YES
+        return true; // displayMenu-->YES
     }
 
-    // TODO (11) Override onOptionsItemSelected
-    // TODO (12) Within onOptionsItemSelected, get the ID of the item that was selected
-    // TODO (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
-    // TODO (14) Don't forgot to call .show() on your Toast
-    // TODO (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        boolean menuItemHandled = super.onOptionsItemSelected(menuItem);
+
+        if (R.id.action_search == menuItem.getItemId()) {
+            menuItemHandled = true;
+            Toast toast = createSearchToast();
+            toast.show();
+        }
+
+        return menuItemHandled;
+    }
+
+    private Toast createSearchToast() {
+        Context context = getApplicationContext();
+        CharSequence text = "Searching GitHub...";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        return toast;
+    }
 }
