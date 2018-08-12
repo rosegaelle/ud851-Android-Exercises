566a88ccbbad4d8045674cd820bc801f81caa48c
MIrrorIMage <MIrrorIMage@rosegaelle.com>
2018-08-11 23:03:46 -0400

    T02.06 Minor fix.

--- Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.06-Exercise-AddPolish/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -88,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
+            super.onPreExecute();
             mProgressBar.setVisibility(View.VISIBLE);
         }
 
