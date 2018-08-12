6e5b38a76d22333fd5bbd641f0b84b7983663146
MIrrorIMage <MIrrorIMage@rosegaelle.com>
2018-08-11 20:25:24 -0400

    T02.05 TODO # 1.

---
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/GithubQueryTask.java
@@ -0,0 +1,12 @@
+package com.example.android.datafrominternet;
+
+import android.os.AsyncTask;
+
+
+public class GithubQueryTask extends AsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
--- Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -66,7 +66,6 @@ public class MainActivity extends AppCompatActivity {
         // TODO (4) Create a new GithubQueryTask and call its execute method, passing in the url to query
     }
 
-    // TODO (1) Create a class called GithubQueryTask that extends AsyncTask<URL, Void, String>
     // TODO (2) Override the doInBackground method to perform the query. Return the results. (Hint: You've already written the code to perform the query)
     // TODO (3) Override onPostExecute to display the results in the TextView
 
